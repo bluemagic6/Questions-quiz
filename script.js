235d2782f423a7dac64d5dfc532b179cdc9e449f
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-05-25 16:56:25 -0500

    added variables for highscore timer countdown and start quiz

---
+++ script.js
@@ -0,0 +1,5 @@
+// variables fro DOM elements
+var highscoresEl = document.getElementById("highscores");
+var timerEl = document.getElementById("timer");
+var countdownEl = document.getElementById("countdown");
+var startquizEl = document.getElementByClassName("start-quiz");
