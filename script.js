66ef80718bc4d87d2c38db29abcd8586162ac6b2
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-06-10 17:33:10 -0500

    added array to hold objects for the qustions

--- script.js
+++ script.js
@@ -3,3 +3,36 @@ var highscoresEl = document.getElementById("highscores");
 var timerEl = document.getElementById("timer");
 var countdownEl = document.getElementById("countdown");
 var startquizEl = document.getElementByClassName("start-quiz");
+
+// varible to hold array of objects with question choices and answers
+
+var questionArr = [
+    {
+        quote: "Commonly used data types DO NOT include:",
+        choices: ["strings", "alerts", "booleans", "numbers"],
+        correct: "alerts"
+    },
+    {
+        quote: "Commonly used data types DO NOT include:",
+        choices: ["strings", "alerts", "booleans", "numbers"],
+        correct: "alerts"
+    },
+    {
+        quote: "Commonly used data types DO NOT include:",
+        choices: ["strings", "alerts", "booleans", "numbers"],
+        correct: "alerts"
+    },
+    {
+        quote: "Commonly used data types DO NOT include:",
+        choices: ["strings", "alerts", "booleans", "numbers"],
+        correct: "alerts"
+    },
+    {
+        quote: "Commonly used data types DO NOT include:",
+        choices: ["strings", "alerts", "booleans", "numbers"],
+        correct: "alerts"
+    },
+] 
+
+// Start timer function 
+
